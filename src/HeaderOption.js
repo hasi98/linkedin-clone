125970f3637be813cb7e6a78e1cda9220481bf52
Hasith <whlmuthukumara@gmail.com>
2023-06-08 07:38:13 +0530

    initial commit

---
+++ src/HeaderOption.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./HeaderOption.css"
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+function HeaderOption({ Icon, title, avatar }) {
+  return (
+    <div className='headerOption'>
+      {Icon && <Icon className='headerOption__icon' />}
+      {avatar && <AccountCircleIcon className='headerOption__icon' src={avatar}/>}
+      <h3 className='headerOption__title'>{title}</h3>
+    </div>
+  )
+}
+
+export default HeaderOption
